fa0335a76e12de73481cf0cd8e4ae68d9cb132e8
AnhNT <anhtuannguyen10012004@gmail.com>
2025-09-22 21:32:28 +0700

    cấu hình lại rn

--- mobile/app/(auth)/login.tsx
+++ mobile/app/(auth)/login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Button } from "react-native";
 import { useRouter } from "expo-router";
+import '../../global.css';
 
 export default function LoginScreen() {
     const router = useRouter();
@@ -17,12 +18,12 @@ export default function LoginScreen() {
     };
 
     return (
-        <View className="flex-1 items-center justify-center bg-white px-4">
-            <Text className="text-2xl font-bold mb-6">Đăng nhập</Text>
+        <View className="flex-1 items-center justify-center bg-teal-50- px-4">
+            <Text className="text-2xl font-bold mb-6">Welcom to Strength Level</Text>
 
             <TextInput
-                className="border w-full px-3 py-2 rounded mb-4"
-                placeholder="Username"
+                className="border w-full px-3 py-2 mb-4 rounded-sm "
+                placeholder="Your email"
                 value={username}
                 onChangeText={setUsername}
             />
@@ -39,8 +40,11 @@ export default function LoginScreen() {
                 className="bg-blue-500 px-4 py-2 rounded w-full items-center"
                 onPress={handleLogin}
             >
-                <Text className="text-white font-semibold">Loginâdadadadaad</Text>
+                <Text className="text-white font-semibold">Login</Text>
             </TouchableOpacity>
+
+            <Button title="Test button"></Button>
+
         </View>
     );
 }

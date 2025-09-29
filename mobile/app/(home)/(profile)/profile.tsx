d30a99a24d1377a437a9fb211ce915fe146dfd1d
duongthee <109543187+duongthee@users.noreply.github.com>
2025-09-29 21:57:26 +0700

    update layout v2

---
+++ mobile/app/(home)/(profile)/profile.tsx
@@ -0,0 +1,19 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function UserTest() {
+    const router = useRouter();
+
+    return (
+        <View className="flex-1 items-center justify-center bg-green-100">
+            <Text className="text-xl font-bold mb-6">Tuấn Anh</Text>
+
+            <TouchableOpacity
+                className="bg-red-500 px-4 py-2 rounded"
+                onPress={() => router.replace("/(auth)/login")}
+            >
+                <Text className="text-white font-semibold">ALO 123</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}

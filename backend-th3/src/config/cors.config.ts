c25bf70a80a89cde0143884ec796e0565e0a804c
AnhNT <anhtuannguyen10012004@gmail.com>
2025-10-20 21:27:31 +0700

    refresh

--- backend-th3/src/config/cors.config.ts
+++
@@ -1,8 +0,0 @@
-import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
-
-export const corsConfig: CorsOptions = {
-    origin: ['*'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
-}
--- th-3/config/env.config.ts
+++
@@ -1,6 +0,0 @@
-export const key = {
-    apiUrl: String(process.env.EXPO_PUBLIC_API_URL),
-    apiHost: String(process.env.EXPO_PUBLIC_API_HOST),
-    apiPort: String(process.env.EXPO_PUBLIC_API_PORT)
-}
-export const API_URL =  `${key.apiHost}:${key.apiPort}/api`;
